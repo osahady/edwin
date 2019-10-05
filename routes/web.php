2b57ead4e6face94ed5169f57ce395e9ce6cfe3d
Osa Hady <osahady@gmail.com>
2019-10-05 14:34:41 +0300

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,38 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+
+*/
+// Route::get('/insert', function(){
+//     DB::insert('INSERT INTO posts(title, content) VALUES (?, ?)', ['PHP', 'laravel is good framework and awesome']);
+
+// });
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+
+*/
+// Route::get('/read', function(){
+//     $results = DB::select('SELECT * FROM posts WHERE id = ?', [1]);
+//     return $results;
+//     // foreach ($results as $post ) {
+//     //     return $post->title ;
+//     // }
+// });
+
+// Route::get('/update', function(){
+//     $updated = DB::update('UPDATE posts SET title="updated title" WHERE id = 1');
+// });
+
+Route::get('/delete', function(){
+    $deleted = DB::delete('DELETE FROM posts WHERE id = ?', [2]);
+    return $deleted;
 });
