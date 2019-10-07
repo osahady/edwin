7b2504349b898dae98fb763bd1bdbd9d250eb416
Osa Hady <osahady@gmail.com>
2019-10-07 11:42:32 +0300

    relations

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function post()
+    {
+        return $this->hasOne('App\Post');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Post;
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,10 +21,10 @@
 |--------------------------------------------------------------------------
 
 */
-// Route::get('/insert', function(){
-//     DB::insert('INSERT INTO posts(title, content) VALUES (?, ?)', ['PHP', 'laravel is good framework and awesome']);
+Route::get('/insert', function(){
+    DB::insert('INSERT INTO posts(title, content, user_id) VALUES (?, ?, ?)', ['laravel', 'laravel is good framework', 1]);
 
-// });
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -42,7 +43,121 @@
 //     $updated = DB::update('UPDATE posts SET title="updated title" WHERE id = 1');
 // });
 
-Route::get('/delete', function(){
-    $deleted = DB::delete('DELETE FROM posts WHERE id = ?', [2]);
-    return $deleted;
+// Route::get('/delete', function(){
+//     $deleted = DB::delete('DELETE FROM posts WHERE id = ?', [2]);
+//     return $deleted;
+// });
+
+/*
+|--------------------------------------------------------------------------
+ELoquent
+|--------------------------------------------------------------------------
+
+*/
+// Route::get('/find', function(){
+//     $posts = Post::all();
+
+//    foreach ($posts as $post ) {
+//        return $post->title;
+//    }
+// });
+
+
+// Route::get('/findwhere', function(){
+//     $posts = Post::where('id','>=' , 3)->orderBy('id', 'desc')->take(2)->get();
+//     return $posts;
+// } );
+
+// Route::get('/findmore', function(){
+//     // $posts = Post::findOrFail(2);
+//     // return $posts;
+
+//     // $posts = Post::where('users_count', '<', 50)->firstOrFail();
+
+// });
+
+// Route::get('/basicinsert', function(){
+//     $post = new Post;
+//     $post->title = 'New title';
+//     $post->content = 'New content';
+
+//     $post->save();
+// });
+
+Route::get('/create', function(){
+    Post::create(['title' => 'title three', 'content'=>'Wow I am learning with Edwin']);
 });
+
+// Route::get('/update', function(){
+//     Post::where('id', 3)->where('is_admin', 0)->update(['title'=>'New PHP', 'content'=>'content2']);
+// });
+
+// Route::get('/delete', function(){
+//     $post = Post::find(1);
+//     $post->delete();
+// });
+
+// Route::get('/delete2', function(){
+//     Post::destroy([4,5]);
+
+//     // Post::where('is_admin', 0)->delete();
+// });
+
+Route::get('/softdeletes', function(){
+    Post::find(10)->delete();
+});
+
+// Route::get('/readsoftdeletes', function(){
+//     // $post = Post::find(11);
+//     // return $post;
+
+//     // $post = Post::withTrashed()->where('id', 10)->get();
+//     // return $post;
+
+//     $post = Post::onlyTrashed()->get();
+//     return $post;
+// });
+
+// Route::get('/restore', function(){
+//     Post::withTrashed()->where('is_admin', 0)->restore();
+// });
+
+// Route::get('/forcedelete', function(){
+//     Post::onlyTrashed()->where('is_admin', 0)->forceDelete();
+// });
+
+/*
+|--------------------------------------------------------------------------
+ELoquent Relationships
+|--------------------------------------------------------------------------
+|
+*/
+//One to One relationship
+// Route::get('/user/{id}', function($id){
+
+//     return User::find($id)->post;
+// })->name('aaa');
+
+// Route::get('/post/{id}/user', function($id){
+//     return Post::find($id)->user;
+// });
+
+// Route::get('/posts', function(){
+//     $user = User::find(1);
+
+//     $posts =[];
+//     foreach ($user->posts as $post) {
+//         array_push($posts, $post);
+//     }
+//     return $posts;
+// });
+
+// Route::get('/posts', function(){
+//     $user = User::find(1);
+
+
+//     foreach ($user->posts as $post) {
+//        echo $post->title. '<br/>';
+//     }
+
+// });
