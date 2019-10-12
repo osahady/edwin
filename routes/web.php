1e0ed9ac4c1f404dc0e867883cca7c3191bd8dd4
Osahady <osahady@gmail.com>
2019-10-12 14:15:13 +0300

    u2

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use App\Country;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 /*
 |--------------------------------------------------------------------------
 | Web Routes
